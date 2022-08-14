c0897b206349f5a0195339a6e31143009a788ee7
minipong-dev <60086046+minipong-dev@users.noreply.github.com>
2022-08-14 00:27:47 -0700

    Add files via upload

--- nft-minting-app-main (dev)/src/App.js
+++ nft-minting-app-main (dev)/src/App.js
@@ -546,147 +546,47 @@ function App() {
                     <s.SpacerMedium />
                     <s.Container>
                     </s.Container>
-                    <div class="wrapper">
-                        <div id="project-grid" class="grid-4_md-2_sm-1 project-grid small-gutter">
-                            <div class="col project project--hover-2 web-design mockups">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/1.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://vimeo.com/139208268" class="lightbox-video"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Video Lightbox</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 print mockups">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/2.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/2_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Branding Mockup</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Mockups</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 web-design graphics print">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/3.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/3_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Chat App</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 web-design graphics print">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/4.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/4_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Money App</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 web-design">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/5.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/5_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Video Chat App</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 web-design">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/6.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/6_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Promo Website</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col project project--hover-2 web-design">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/7.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/7_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">Landing Page</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-
-                            <div class="col project project--hover-2 print">
-                            <div class="project__img-holder">
-                                <img src="http://deothemes.com/envato/canna/html/img/portfolio/8.jpg" alt="" class="project__img"/>
-                                <div class="project__overlay">
-                                <div class="project__icons">
-                                    <a href="http://deothemes.com/envato/canna/html/img/portfolio/8_big.jpg" class="lightbox-img"><i class="fa fa-search"></i></a>
-                                    <a href="portfolio-single.html"><i class="fa fa-link"></i></a>
-                                </div>
-                                </div>
-                                <div class="project__description">
-                                <h3 class="project__title"><a href="portfolio-single.html">TV Show Website</a></h3>
-                                <span class="project__category"><a href="portfolio-single.html">Web Design</a></span>
-                                </div>
-                            </div>
-                            </div>
-
-
-                        </div>
-
-                        <div class="wrapper grid-3 text-center">
-                            <div class="col">
-
-                            </div>
-
-                        </div>
-                    </div>
+                    <div class="container">
+  <h1 class="text-center">Portfolio</h1>
+  <div class="gallery">
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/arch" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/any" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/tech" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/animals" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/people" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+    <div class="view overlay hm-black-light gallery-item">
+      <img src="https://placeimg.com/640/480/any" alt="Placeholder Image" class="img-fluid"/>
+      <div class="mask flex-center">
+        <p class="white-text">Gallery Item</p>
+      </div>
+    </div>
+  </div>
+</div>
 
                 </s.Container>
                 <s.SpacerMedium />
